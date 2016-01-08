441be2979df0f8ab1df2dab39e1999d83c8f3955
Joe Catera <zhouyaoji@yahoo.com>
2016-01-07 23:14:37 -0800

    Added q=<query>.

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
   res.send("ToDo API Root");
 });
 
+// GET /todocs?completed=true&q=house
 app.get('/todos', function(req, res) {
   var queryParams = req.query;
   var filteredTodos = todos;
@@ -19,6 +20,9 @@ app.get('/todos', function(req, res) {
     filteredTodos = _.where(filteredTodos, { completed: true });
   } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
     filteredTodos = _.where(filteredTodos, { completed: false });
+  }
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function(todo) { return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; });
   }
   res.json(filteredTodos);
 });
