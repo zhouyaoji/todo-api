6a81095eb5dbb35e2f7b13e63a13c160f092e90b
Joe Catera <zhouyaoji@yahoo.com>
2016-01-11 22:18:15 -0800

    Deleted the commented old code.

--- server.js
+++ server.js
@@ -39,26 +39,6 @@ app.get('/todos', function(req, res) {
    });
 });
    
-  /*
-  var filteredTodos = todos;
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-    filteredTodos = _.where(filteredTodos, {
-      completed: true
-    });
-  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-    filteredTodos = _.where(filteredTodos, {
-      completed: false
-    });
-  }
-  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    filteredTodos = _.filter(filteredTodos, function(todo) {
-      return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-    });
-  }
-  res.json(filteredTodos);
-});
-
-*/
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id);
   db.todo.findById(todoId).then(function (todo) {
